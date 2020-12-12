831ab77c65280f0fd35d084123851def97f78636
Tega Tega <tegaslink@gmail.com>
2020-12-12 03:13:07 -0800

    8_1

---
+++ Common_Questions/TextBookQuestions/PythonCrashCourse/Chapter_8/8_1.py
@@ -0,0 +1,10 @@
+
+'''
+8-1. Message: Write a function called display_message() that prints one sen- tence telling everyone what you are learning about in this chapter. 
+Call the function, and make sure the message displays correctly.
+'''
+
+def aboutChapter():
+    print("This chapter will teach you about functions")
+
+aboutChapter()
