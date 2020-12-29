8f6d4aad56faaa2c324d9acc56684e1bd0f4d1df
Tega Tega <tegaslink@gmail.com>
2020-12-29 10:31:16 -0500

    Ex_9_2

---
+++ Common_Questions/TextBookQuestions/PythonCrashCourse/Chapter_9/Ex_9_2.py
@@ -0,0 +1,22 @@
+
+'''
+9-2. Three Restaurants: Start with your class from Exercise 9-1. Create three different instances from the class, 
+and call describe_restaurant() for each instance.
+'''
+
+class Restaurant:
+    '''this is the reataurant class'''
+    def __init__(self,restaurant_name,cuisine_type,location):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+        self.location = location
+
+    def describe_restaurant(self):
+        print(f"The name of the restaurant is {self.restaurant_name}, they serve {self.cuisine_type} cuisine and they are located in {self.location}.")
+
+
+
+
+my_restaurant = Restaurant('Eureka','American','Mountain View')
+my_restaurant.describe_restaurant()
+
